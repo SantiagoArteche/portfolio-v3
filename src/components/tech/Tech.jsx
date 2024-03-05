1f94f3fb1b2e504205390872e71a55f66a70ff1f
santiago arteche <santiarteche@hotmail.com>
2024-03-05 05:33:20 -0300

    tech icons changed

--- src/components/tech/Tech.jsx
+++ src/components/tech/Tech.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import { IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
-import { IoLogoReact, IoLogoNodejs, IoLogoFirebase } from "react-icons/io5";
+import { IoLogoReact, IoLogoNodejs } from "react-icons/io5";
 import { FaGitAlt, FaGithub } from "react-icons/fa";
 import { BsBootstrapFill } from "react-icons/bs";
-import { SiJavascript, SiMongodb } from "react-icons/si";
+import { SiJavascript, SiMongodb, SiMui } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
+import { TbBrandRedux } from "react-icons/tb";
+
 export const Tech = () => {
   return (
     <Box
@@ -34,10 +36,11 @@ export const Tech = () => {
       <BsBootstrapFill color="#9461fb" />
       <SiJavascript color="yellow" />
       <IoLogoReact color="rgb(13,202,240)" />
+      <SiMui color="#1e88e5" />
+      <TbBrandRedux color="#533484" />
       <IoLogoNodejs color="green" />
       <SiExpress color="white" />
       <SiMongodb color="green" />
-      <IoLogoFirebase color="yellow" />
       <FaGitAlt color="#dc3545" />
       <FaGithub color="white" />
     </Box>
