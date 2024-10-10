cde89c6674ad60272129cf87c8e7e630c03f1e14
santiago arteche <santiarteche@hotmail.com>
2024-10-09 23:38:28 -0300

    cv changed

--- src/components/cv/Cv.jsx
+++ src/components/cv/Cv.jsx
@@ -1,10 +1,19 @@
 import { Box } from "@mui/material";
+import { useContext } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
+
+import { LanguageContext } from "../../Context/LanguageContext";
 export const Cv = () => {
+  const { language } = useContext(LanguageContext);
+
   return (
     <Box
       component={"a"}
-      href="https://www.canva.com/design/DAFzI5XREOg/354jK1_bOsq43ri1FKa6Eg/edit?utm_content=DAFzI5XREOg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+      href={
+        language === "ES"
+          ? `/CV-SANTIAGOARTECHE.pdf`
+          : `/CV-SANTIAGOARTECHE-EN.pdf`
+      }
       target="_blank"
       aria-label="cv-link"
       sx={{
