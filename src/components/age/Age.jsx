d492e6d89651f50d9f9ab9889188df26a6ab8da9
Santiago Arteche <santiagoarteche7@gmail.com>
2024-10-13 22:26:56 -0300

    Update Age.jsx

--- src/components/age/Age.jsx
+++ src/components/age/Age.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { AgePresentational } from "./AgePresentational";
 
 export const Age = ({ language }) => {
-  let [age, setAge] = useState(16);
+  let [age, setAge] = useState(17);
 
   useEffect(() => {
     const interval = setInterval(() => {
